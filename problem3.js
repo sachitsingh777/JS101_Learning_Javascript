1089d68c27f985581f89e0c6f4e25a71633cd464
unknown <sachitsinghbais777@gmail.com>
2022-10-11 18:31:50 +0530

    problem

---
+++ problem3.js
@@ -0,0 +1,6 @@
+let x="name";
+  name="sacchitanand singh";
+console.log(x,name);
+y="age";
+age=26;
+console.log(y,age);
---
+++ problem4.js
@@ -0,0 +1,27 @@
+let x="name:-";
+  name="abc";
+console.log(x,name);
+y="school:-"
+school="xyz school";
+console.log(y,school);
+z="grade:-"
+grade=5;
+console.log(z,grade);
+b="section:-"
+section="A";
+console.log(b,section);
+c="rollno:-"
+rollno=01;
+console.log(c,rollno);
+d="subject";
+e="marks";
+console.log(d,e);
+subject1="mathemattics:-";
+mathematics="☺";
+console.log(subject1,mathematics);
+subject2="english:-";
+english="☺";
+console.log(subject2,english);
+subject3="hindi:-";
+hindi="♠";
+console.log(subject3,hindi);
